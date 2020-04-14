c72f29b59ca589cd42e162522284fa800903a3c1
이정현 <fkdlrj1250@gmail.com>
2020-04-14 16:54:45 +0900

    200414-1

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-function Food({ name, picture, rating }) {
+/* function Food({ name, picture, rating }) {
   return (
     <div>
       <h1>I like {name}</h1>
@@ -54,6 +53,31 @@ function App() {
       ))}
     </div>
   );
+} */
+
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({
+      count: current.count + 1 /* this.state.count + 1 */
+    }));
+  };
+  minus = () => {
+    this.setState(current => ({
+      count: current.count - 1 /* this.state.count - 1 */
+    }));
+  };
+  render() {
+    return (
+      <div>
+        <h1>The number is : {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
