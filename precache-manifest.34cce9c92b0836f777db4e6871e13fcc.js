1884f6e8f7adeaced96749224ece6a137e24931f
이정현 <fkdlrj1250@gmail.com>
2020-04-22 16:51:11 +0900

    Updates

---
+++ precache-manifest.34cce9c92b0836f777db4e6871e13fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73ccb8c85b4e2d0e985456666fc6b64c",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "057f16b51ae0ebda4d57",
+    "url": "/movie_app/static/css/main.5721af45.chunk.css"
+  },
+  {
+    "revision": "1d2d5261e5e33a966d0f",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.111c2b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "057f16b51ae0ebda4d57",
+    "url": "/movie_app/static/js/main.a7df48f8.chunk.js"
+  },
+  {
+    "revision": "c4e503dc5fc5f4a3fb01",
+    "url": "/movie_app/static/js/runtime-main.202e151f.js"
+  }
+]);
